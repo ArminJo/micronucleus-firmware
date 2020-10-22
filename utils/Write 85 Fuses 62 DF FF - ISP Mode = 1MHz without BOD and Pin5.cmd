054a0bf0e9ccccd8e421e8d2ecb594b6731b1f83
Armin <armin.arduino@gmail.com>
2020-10-22 22:24:30 +0200

    Closes #15

--- utils/Write 85 Fuses 62 DF FF - ISP Mode = 1MHz without BOD and Pin5.cmd
+++ utils/Write 85 Fuses 62 DF FF - ISP Mode = 1MHz without BOD and Pin5.cmd
@@ -11,10 +11,10 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 @echo.Writing ATtiny85 Lfuse to 0x62 - 8MHz clock, Clock div by 8 -> CPU clock = 1 MHz, 6 Clocks from Power Down sleep + 64 ms after reset.
 @echo Writing ATtiny85 Hfuse to 0xDF - External Reset pin enabled (Pin5 not usable as I/O) + BOD disabled + (digispark default) Enable Serial Program and Data Downloading.
-@echo Writing ATtiny85 EFuse to 0xFF - self programming disabled - no bootloader function possible.
+@echo Writing ATtiny85 EFuse to 0xFE - self programming enabled.
 @echo.
 @echo trying to connect to device...
-avrdude -p ATtiny85 -c stk500v1 -P COM6  -b 19200 -Ulfuse:w:0x62:m -Uhfuse:w:0xDF:m -Uefuse:w:0xFF:m
+avrdude -p ATtiny85 -c stk500v1 -P COM6  -b 19200 -Ulfuse:w:0x62:m -Uhfuse:w:0xDF:m -Uefuse:w:0xFE:m
 
 pause
 exit
