85b8664abf559e12b9ba6732fe47739b2e197f70
Armin <armin.arduino@gmail.com>
2021-11-05 12:57:36 +0100

    Bumped binaries version to 2.6

--- utils/0_Micronucleus-info.cmd
+++ utils/0_Micronucleus-info.cmd
@@ -1,3 +1,3 @@
 @echo Run micronucleus with the --info flag
-..\commandline\builds\x86_64-mingw32\micronucleus --info
+..\commandline\builds\x86_64-mingw32\micronucleus --no-ansi --info --timeout 60
 pause
